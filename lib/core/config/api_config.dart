f72240370311c8769912256e25206f21f6a89cd1
AxionSoftware-Inc <shaxzodturayev123@gmail.com>
2026-07-11 01:37:52 +0500

    Use production API by default in debug builds

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,28 +1,10 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
-
 class ApiConfig {
   static const _productionBase = 'http://91.108.121.56/api';
-  static const _desktopDebugBase = 'http://127.0.0.1:8000/api';
-  static const _androidEmulatorDebugBase = 'http://10.0.2.2:8000/api';
 
   static String get baseUrl {
     const fromDefine = String.fromEnvironment('API_BASE_URL');
     if (fromDefine.isNotEmpty) {
       return fromDefine;
-    }
-
-    if (!kIsWeb && kDebugMode) {
-      if (Platform.isAndroid) {
-        return _androidEmulatorDebugBase;
-      }
-      if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
-        return _desktopDebugBase;
-      }
-      if (Platform.isIOS) {
-        return _desktopDebugBase;
-      }
     }
 
     return _productionBase;
