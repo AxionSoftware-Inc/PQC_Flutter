d03598ae021d9a5bc21efb52d3cc9cc0a8828b2d
AxionSoftware-Inc <shaxzodturayev123@gmail.com>
2026-07-14 21:04:09 +0500

    wire v3 codec adapters into engine module

---
+++ lib/features/crypto/v3/v3_codec_adapters.dart
@@ -0,0 +1,1 @@
+export 'package:crypto_core/src/crypto/v3/v3_codec_adapters.dart';
--- packages/crypto_core/lib/crypto_core.dart
+++ packages/crypto_core/lib/crypto_core.dart
@@ -30,6 +30,7 @@ export 'src/crypto/v3/pqc_v3_crypto_adapter.dart';
 export 'src/crypto/v3/v3_message_codecs.dart';
 export 'src/crypto/v3/v3_attachment_codec.dart';
 export 'src/crypto/v3/v2_compatibility_decoder.dart';
+export 'src/crypto/v3/v3_codec_adapters.dart';
 export 'src/crypto/group_key_store.dart';
 export 'src/crypto/message_codec.dart';
 export 'src/crypto/outbound_message_cache.dart';
