3dc3f10662fadf2141b938d158547bf425e32ef7
AxionSoftware-Inc <shaxzodturayev123@gmail.com>
2026-08-05 15:15:44 +0500

    feat(rbac): add workforce lifecycle administration

--- backend_plugins/rbac/urls.py
+++ backend_plugins/rbac/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import InvitationCreateView, MemberAssignmentView, MemberDeactivateView, MemberListView, RbacMeView, RoleDetailView, RoleListCreateView
+from .views import DefaultRoleBootstrapView, InvitationCreateView, MemberAssignmentView, MemberDeactivateView, MemberListView, MemberReactivateView, RbacMeView, RoleDetailView, RoleListCreateView
 
 urlpatterns = [
     path('me', RbacMeView.as_view()),
     path('roles', RoleListCreateView.as_view()),
     path('roles/<int:role_id>', RoleDetailView.as_view()),
+    path('roles/bootstrap-defaults', DefaultRoleBootstrapView.as_view()),
     path('members', MemberListView.as_view()),
     path('members/<int:member_id>/role', MemberAssignmentView.as_view()),
     path('members/<int:member_id>/deactivate', MemberDeactivateView.as_view()),
+    path('members/<int:member_id>/reactivate', MemberReactivateView.as_view()),
     path('invitations', InvitationCreateView.as_view()),
 ]
